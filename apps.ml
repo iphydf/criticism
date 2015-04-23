16c1472598729df634b199669803f0cc81ca9492
iphydf <iphydf@users.noreply.github.com>
2015-04-23 18:28:44 +0100

    Print list of apps on /

--- apps.ml
+++ apps.ml
@@ -3,20 +3,29 @@ open Async.Std
 open Cohttp_async
 
 
+let rec apps = [
+  "/apidsl", Apidsl.process;
+  "/", list_apps;
+]
+
+and list_apps body =
+  "List of apps:\n" ^ String.concat (List.map ~f:(fun (n, _) -> "  " ^ n ^ "\n") apps)
+
+
 let process_error path body =
   "Not found: " ^ path ^ "\n"
 
-
-let process = function
-  | "/apidsl" -> Apidsl.process
-  | path -> process_error path
+let process path =
+  match List.Assoc.find apps path with
+  | Some handler -> handler
+  | _ -> process_error path
 
 
 let start_server port () =
   Server.create ~on_handler_error:`Raise
     (Tcp.on_port port) (fun ~body _ req ->
         match req |> Cohttp.Request.meth with
-        | `POST ->
+        | `POST | `GET ->
             Body.to_string body >>= fun body ->
             let app = Uri.path @@ Cohttp.Request.uri req in
             let body = process app body |> Body.of_string in
