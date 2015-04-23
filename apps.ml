061c85ab9764e932e7cc05b33323b23613b67489
iphydf <iphydf@users.noreply.github.com>
2015-04-23 15:23:00 +0100

    -

--- apps.ml
+++ apps.ml
@@ -2,29 +2,14 @@ open Core.Std
 open Async.Std
 open Cohttp_async
 
-let parse input =
-  let lexbuf = Lexing.from_string input in
-
-  let state = ApiLexer.state () in
-  let api = ApiParser.parse_api (ApiLexer.token state) lexbuf in
-
-  api
-
-
-let apigen input =
-  try
-    let ApiAst.Api (pre, api, post) = parse input in
-    ApiPasses.all pre api post
-  with e ->
-    "// Error: " ^ Exn.to_string e ^ "\n"
-
 
 let start_server port () =
   Server.create ~on_handler_error:`Raise
     (Tcp.on_port port) (fun ~body _ req ->
         match req |> Cohttp.Request.meth with
         | `POST ->
-          let body = Body.map body apigen in
+          Body.to_string body >>= fun body ->
+          let body = Apidsl.process body |> Body.of_string in
           Server.respond ~body `OK
         | _ -> Server.respond `Method_not_allowed
       )
--- apps/apidsl.ml
+++ apps/apidsl.ml
@@ -1,1 +1,18 @@
-let passes = ApiPasses.all
+open Core.Std
+
+
+let parse input =
+  let lexbuf = Lexing.from_string input in
+
+  let state = ApiLexer.state () in
+  let api = ApiParser.parse_api (ApiLexer.token state) lexbuf in
+
+  api
+
+
+let process input =
+  try
+    let ApiAst.Api (pre, api, post) = parse input in
+    ApiPasses.all pre api post
+  with e ->
+    "// Error: " ^ Exn.to_string e ^ "\n"
